64f3e2234a0074d114c630c8617fcc4a86e83046
feuillelisse <nisashadrina@gmail.com>
2021-03-15 12:59:52 +0700

    Update WeeklySales.java

--- sorting/WeeklySales.java
+++ sorting/WeeklySales.java
@@ -5,28 +5,39 @@
  */
 package sorting;
 import java.util.Scanner;
-
-// ************************************************************* 
-// WeeklySales.java 
-// 
-// Sorts the sales staff in descending order by sales. 
-// ************************************************************ 
-public class WeeklySales { 
-    public static void main(String[] args) { 
-        Salesperson[] salesStaff = new Salesperson[10]; 
-        salesStaff[0] = new Salesperson("Jane", "Jones", 3000); 
-        salesStaff[1] = new Salesperson("Daffy", "Duck", 4935); 
-        salesStaff[2] = new Salesperson("James", "Jones", 3000); 
-        salesStaff[3] = new Salesperson("Dick", "Walter", 2800); 
-        salesStaff[4] = new Salesperson("Don", "Trump", 1570); 
-        salesStaff[5] = new Salesperson("Jane", "Black", 3000); 
-        salesStaff[6] = new Salesperson("Harry", "Taylor", 7300); 
-        salesStaff[7] = new Salesperson("Andy", "Adams", 5000); 
-        salesStaff[8] = new Salesperson("Jim", "Doe", 2850); 
-        salesStaff[9] = new Salesperson("Walt", "Smith", 3000); 
-        Sorting.insertionSort(salesStaff); 
-        System.out.println ("\nRanking of Sales for the Week\n"); 
-        for (Salesperson s : salesStaff) 
-        System.out.println (s); 
-    } 
-} 
+// *************************************************************
+// WeeklySales.java
+//
+// Sorts the sales staff in descending order by sales.
+// ************************************************************
+public class WeeklySales{
+    public static void main(String[] args){
+        Salesperson[] salesStaff;
+        String[] firstName;
+        String[] lastName;
+        int salesAmt;
+        int size;
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("\nHow many salesperson do you want to input? ");
+        size = scan.nextInt();
+        salesStaff = new Salesperson[size];
+        firstName = new String[size];
+        lastName = new String[size];
+        
+        System.out.print("\nEnter the salesperson data : ");
+        for (int i = 0; i < size; i++){
+            System.out.print("\nFirst Name : ");
+            firstName[i] = scan.next();
+            System.out.print("Last Name : ");
+            lastName[i] = scan.next();
+            System.out.print("Total Sales this week : ");
+            salesAmt = scan.nextInt();
+            salesStaff[i] = new Salesperson(firstName[i], lastName[i], salesAmt);
+        }
+        Sorting.insertionSort(salesStaff);
+        System.out.println ("\nRanking of Sales for the Week\n");
+        for (Salesperson s : salesStaff)
+            System.out.println (s);
+    }
+}
