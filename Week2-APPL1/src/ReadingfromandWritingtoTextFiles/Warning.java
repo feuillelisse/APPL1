5808069da29a4b92bf2c10d39929ea2dd0bc456c
feuillelisse <nisashadrina@gmail.com>
2021-03-24 08:15:34 +0700

    Menyelesaikan soal Read and Write

--- Week2-APPL1/src/ReadingfromandWritingtoTextFiles/Warning.java
+++ Week2-APPL1/src/ReadingfromandWritingtoTextFiles/Warning.java
@@ -24,38 +24,44 @@ public class Warning{
     public static void main (String[] args){ 
         int creditHrs; // number of semester hours earned 
         double qualityPts; // number of quality points earned 
-        double gpa; // grade point (quality point) average 
-        String line, name;
+        double gpa; // grade point (quality point) average
+        String name;
         String inputName = "D:\\Github\\APPL1\\Week2-APPL1\\src\\ReadingfromandWritingtoTextFiles\\students.dat"; 
         String outputName = "D:\\Github\\APPL1\\Week2-APPL1\\src\\ReadingfromandWritingtoTextFiles\\warning.dat";
         Scanner inputFile = null;
-        FileWriter outFile = null;
+        PrintWriter outFile = null;
         try{ 
             // Set up scanner to input file 
             inputFile = new Scanner(new File(inputName));
             // Set up the output file stream 
-            outFile = new FileWriter(outputName);
+            outFile = new PrintWriter(new FileWriter(outputName));
             // Print a header to the output file 
-            outFile.println("Students on Academic Warning");
-            outFile.println ();              
-            outFile.println (); 
+            outFile.println("Students on Academic Warning\n");
             // Process the input file, one token at a time 
-
-            while (){ 
-                // Get the credit hours and quality points and 
-                // determine if the student is on warning. If so, 
-                // write the student data to the output file. 
+            while(inputFile.hasNext()){ 
+                // Get the credit hours and quality points
+                String[] line = inputFile.nextLine().split(" ");
+                name = line[0];
+                creditHrs = Integer.parseInt(line[1]); 
+                qualityPts = Double.parseDouble(line[2]);
+                // determine if the student is on warning
+                // If so, write the student data to the output file.
+                gpa = qualityPts / creditHrs;
+                if(gpa < 1.5 && creditHrs < 30 || gpa < 1.75 && creditHrs < 60 || gpa < 2.0){
+                    outFile.println(name + " " + creditHrs + " " + gpa);
+                }
             } 
-            // Close output file 
+            // Close output file
+            outFile.close();
         } 
         catch (FileNotFoundException exception){ 
             System.out.println ("The file " + inputName + " was not found."); 
         } 
         catch (IOException exception){ 
-            System.out.println (exception); 
+            System.out.println ("Something else goes wrong with the input or output stream."); 
         } 
         catch (NumberFormatException e){ 
-            System.out.println ("Format error in input file: " + e); 
+            System.out.println ("Format error in input file: " + e);
         } 
     } 
 }
