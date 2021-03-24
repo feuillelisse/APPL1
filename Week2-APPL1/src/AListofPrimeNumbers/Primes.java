24820542000b06aa662cad96a4c974629b516a10
feuillelisse <nisashadrina@gmail.com>
2021-03-24 22:51:34 +0700

    Finished primes

---
+++ Week2-APPL1/src/AListofPrimeNumbers/Primes.java
@@ -0,0 +1,24 @@
+/*
+ * Nama             : Nisa Shadrina
+ * NIM              : 191524053
+ * Hari, Tanggal    : Rabu, 23 Maret 2021
+ */
+package AListofPrimeNumbers;
+// ***************************************************************** 
+// Primes.java 
+// Generates a list of primes less than or equal to the integer 
+// input by the user. 
+// ***************************************************************** 
+import java.awt.*; 
+import javax.swing.*; 
+public class Primes{ 
+    public static void main (String[] args){ 
+        JFrame frame = new JFrame ("Primes"); 
+        frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE); 
+
+        PrimePanel primePanel = new PrimePanel (); 
+        frame.getContentPane().add(primePanel); 
+        frame.pack(); 
+        frame.setVisible(true); 
+    } 
+}
